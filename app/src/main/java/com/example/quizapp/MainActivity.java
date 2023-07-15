35857c31a7498557de75e5ac34d8d69dee513e50
Divu <divyeshkuchhadia@gmail.com>
2023-07-16 00:36:48 +0530

    About and Home Page got linked

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button about;
+    Button start;
+    AutoCompleteTextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        about = (Button)findViewById(R.id.aboutButton);
+        about.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(getApplicationContext(),AboutUs.class);
+                startActivity(i);
+            }
+        });
     }
 }
