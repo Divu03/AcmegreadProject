57759e8e900dec74efc721873957937fbff977f5
Divu <divyeshkuchhadia@gmail.com>
2023-07-17 00:16:59 +0530

    little bit more ui added

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -7,18 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button about;
     Button start;
-    AutoCompleteTextView name;
+    EditText name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         about = (Button)findViewById(R.id.aboutButton);
         about.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(getApplicationContext(),AboutUs.class);
+                startActivity(i);
+            }
+        });
+        start = (Button)findViewById(R.id.startButton);
+        start.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent i = new Intent(getApplicationContext(),AboutUs.class);
