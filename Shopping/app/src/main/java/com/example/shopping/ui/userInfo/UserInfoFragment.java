9a3d0ddce953ccd6538481673f7305f0104b0d3b
Divu <divyeshkuchhadia@gmail.com>
2023-08-19 21:41:02 +0530

    user info UI backend ready

--- Shopping/app/src/main/java/com/example/shopping/ui/userInfo/UserInfoFragment.java
+++ Shopping/app/src/main/java/com/example/shopping/ui/userInfo/UserInfoFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.shopping.HomeActivity;
+import com.example.shopping.R;
 import com.example.shopping.databinding.FragmentUserInfoBinding;
 
 public class UserInfoFragment extends Fragment {
 
     private FragmentUserInfoBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,10 +29,18 @@ public class UserInfoFragment extends Fragment {
 
         binding = FragmentUserInfoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Button bhome  = binding.bHome;
+        bhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.nav_home);
+            }
+        });
 
-        final TextView textView = binding.textGallery;
-        userInfoViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
     @Override
